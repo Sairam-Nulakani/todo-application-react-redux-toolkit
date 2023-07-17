62b4a3b3c82519d87624b1d4e8e775894a9a9391
“Sairam-Nulakani” <sairamnulakani4@gmail.com>
2023-07-17 21:09:07 +0530

    redux tool kit intro

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex  justify-center">
+      <h1 className="text-4xl font-bold py-6">
+        {" "}
+        Todo Application - Redux Toolkit
+      </h1>
+    </div>
+  );
+}
+
+export default App;
